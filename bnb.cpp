bd89b1a1356e3e34efb8d2f2573f9bb9ada86a4f
GlitchBox <glitchbox29@gmail.com>
2020-12-06 09:41:32 +0600

    donezo

--- bnb.cpp
+++ bnb.cpp
@@ -2,13 +2,12 @@
 #define inf INT_MAX
 using namespace std;
 
-typedef pair<int, vector<int> > pair_int_arr;
+typedef pair<int, vector<vector<int> > > pair_int_arr;
 typedef pair<int, pair_int_arr> pair_int_pair;
 
-// int graph[1000][1000];
 vector<vector<int> > graph(1000);
 
-int rowReduction(int costMatrix[][1000], int nodeNo){
+int rowReduction(vector<vector<int> > &costMatrix, int nodeNo){
 
 	int totalReduction = 0;
 	for(int i=0;i<nodeNo;i++){
@@ -33,7 +32,7 @@ int rowReduction(int costMatrix[][1000], int nodeNo){
 	return totalReduction;
 }
 
-int colReduction(int costMatrix[][1000], int nodeNo){
+int colReduction(vector<vector<int> > &costMatrix, int nodeNo){
 
 	int totalReduction = 0;
 	for(int j=0;j<nodeNo;j++){
@@ -57,9 +56,8 @@ int colReduction(int costMatrix[][1000], int nodeNo){
 	return totalReduction;
 }
 
-int childCost(int parentNode, int parentCost, vector<vector<int> > oldCost , int childNode, priority_queue<pair_int_pair, vector<pair_int_pair>, greater<pair_int_pair> >&pq, int nodeNo){
+int childCost(int parentNode, int parentCost, vector<vector<int> > &oldCost , int childNode, vector<vector<int> > &newCost, int nodeNo){
 
-	int newCost[1000][1000];
 	int cost = oldCost[parentNode][childNode] + parentCost;
 	for(int i=0;i<nodeNo;i++){
 		for(int j=0;j<nodeNo;j++)
@@ -79,20 +77,22 @@ int childCost(int parentNode, int parentCost, vector<vector<int> > oldCost , int
 
 	cost += rowReduction(newCost, nodeNo);
 	cost += colReduction(newCost, nodeNo);
-	pq.push(make_pair(cost, make_pair(childNode, newCost)));
 
 	return cost;
+
 }
 
 int main(){
 
-	priority_queue<pair_int_pair, vector<pair_int_pair>, greater<pair_int_pair> > activeNodes;
+	for(int i=0;i<1000;i++)
+		graph[i] = vector<int>(1000);
 	for(int i=0;i<1000;i++)
 		for(int j=0;j<1000;j++)
 			graph[i][j] = inf;
+	priority_queue<pair_int_pair, vector<pair_int_pair>, greater<pair_int_pair> > activeNodes;	
 	
 	//take in the inputs
-	ifstream myfile("input.txt");
+	ifstream myfile("input1.txt");
 	string line;
 	int nodeNo;
 
@@ -109,29 +109,10 @@ int main(){
 		graph[i][j] = cost;
 	}
 	myfile.close();
-
-	// for(int i=0;i<nodeNo;i++){
-	// 	for(int j=0;j<nodeNo;j++)
-	// 		cout<<graph[i][j]<<" ";
-	// 	cout<<endl;
-	// }
-	// cout<<endl;
 	
 	int rootCost = rowReduction(graph, nodeNo);
-	// graph[0][nodeNo-1] = 2;
-	// for(int i=0;i<nodeNo;i++){
-	// 	for(int j=0;j<nodeNo;j++)
-	// 		cout<<graph[i][j]<<" ";
-	// 	cout<<endl;
-	// }
-	// cout<<endl;
-
 	rootCost += colReduction(graph, nodeNo);
-	// for(int i=0;i<nodeNo;i++){
-	// 	for(int j=0;j<nodeNo;j++)
-	// 		cout<<graph[i][j]<<" ";
-	// 	cout<<endl;
-	// }
+
 	activeNodes.push(make_pair(rootCost, make_pair(0, graph)));
 	int upperBound = inf;
 	
@@ -142,7 +123,6 @@ int main(){
 		activeNodes.pop();
 		int topCost = topNode.first;
 		int currentNode = topNode.second.first;
-		cout<<"topCost,currentNode: "<<topCost<<","<<currentNode<<endl;
 
 		//if topNode is a leaf node
 		bool ifLeaf = false;
@@ -152,30 +132,12 @@ int main(){
 				if(!ifLeaf)
 					ifLeaf = true;
 
-				int childcost = childCost(currentNode, topCost, topNode.second.second, i, activeNodes, nodeNo);
-				// activeNodes.push(make_pair(childcost, make_pair(i,newCost)));
-				// cout<<"children: "<<endl;
-				// for(int j=0;j<nodeNo;j++){
-				// 	for(int k=0;k<nodeNo;k++){
-				// 		if(topNode.second.second[j][k] == inf)
-				// 			cout<<"inf ";
-				// 		else
-				// 			cout<<topNode.second.second[j][k]<<" ";
-				// 	}
-				// 	cout<<endl;
-				// }
-				// cout<<endl;
-				// cout<<"childcost, i: "<<childcost<<","<<i<<endl;
-				// for(int j=0;j<nodeNo;j++){
-				// 		for(int k=0;k<nodeNo;k++){
-				// 		if(newCost[j][k] == inf)
-				// 			cout<<"inf ";
-				// 		else
-				// 			cout<<newCost[j][k]<<" ";
-				// 	}
-				// 		cout<<endl;
-				// }
-				// cout<<endl;
+				vector<vector<int> > newCost(1000);
+				for(int j=0;j<1000;j++)
+					newCost[j] = vector<int>(1000);
+
+				int childcost = childCost(currentNode, topCost, topNode.second.second, i, newCost, nodeNo);
+				activeNodes.push(make_pair(childcost, make_pair(i,newCost)));
 			}
 		}
 
@@ -196,45 +158,3 @@ int main(){
 	cout<<upperBound<<endl;
 
 }
-
-// void func1(int z, priority_queue<pair<int, int(*)[1000]> >&pq){
-
-// 	int x[1000][1000];
-// 	for(int i=0;i<10;i++)
-// 		for(int j=0;j<10;j++)
-// 			x[i][j] = z*100;
-// 	pq.push(make_pair(2,x));
-// }
-
-// void func2(int z, priority_queue<pair<int, vector<vector<int> > > >&pq){
-
-// 	vector< vector<int> > x(10);
-// 	for(int i=0;i<10;i++){
-
-// 		x[i] = vector<int>(10);
-// 	}
-
-// 	for(int i=0;i<10;i++)
-// 		for(int j=0;j<10;j++)
-// 			x[i][j] = z*200;
-// 	pq.push(make_pair(1,x));
-// }
-
-// int main(){
-
-// 	priority_queue<pair<int, vector<vector<int> > > > pq;
-// 	for(int i=1;i<=2;i++)
-// 		func2(i, pq);
-
-// 	while(!pq.empty()){
-// 		pair<int, vector<vector<int> > > topNode = pq.top();
-// 		for(int i=0;i<10;i++){
-// 			for(int j=0;j<10;j++)
-// 				cout<<topNode.second[i][j]<<" ";
-// 			cout<<endl;
-// 		}
-// 		cout<<endl;
-// 		pq.pop();
-// 	}
-
-// }
